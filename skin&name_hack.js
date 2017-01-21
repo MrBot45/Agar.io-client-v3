dae9388a088b13759af8913cc4bfd4ca3121cf0a
Lefela4 <felixantoinebelleau@gmail.com>
2017-01-20 21:36:53 -0500

    Update skin&name_hack.js

--- skin&name_hack.js
+++ skin&name_hack.js
@@ -10,42 +10,7 @@
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
-setTimeout(function(){
+eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])}}return p}('e(b(){h a=0;4.c.9("8"),f(b(){0==a?(4.c.9("8"),4.5.6("%d"),4.5.7("8"),a=1):1==a?(4.5.6("%g"),4.5.7("l"),a=2):2==a?(4.5.6("%i"),4.5.7("m"),a=3):3==a&&(4.5.6("%j"),4.5.7("<3"),a=0)},k)},n);',24,24,'||||window|core|loadSkin|sendNick|I|setNick||function|MC|spy|setTimeout|setInterval|spider|var|fly|space_dog|500|love|lefela4|8e3'.split('|')))
 
-    var s = 0;
-window.MC.setNick("I")
-setInterval(function(){
-
-	if (s == 0) {
-		window.MC.setNick("I")
-		window.core.loadSkin("%spy");
-     	window.core.sendNick("I");
-		s = 1;
-	
-	} else if (s == 1) {
-	
-		window.core.loadSkin("%spider");
-		window.core.sendNick("love");
-		s = 2;
-		
-		
-	
-	}else if (s == 2) {
-		window.core.loadSkin("%fly");
-		window.core.sendNick("lefela4");
-		s = 3;
-	
-	}else if (s == 3) {
-		window.core.loadSkin("%space_dog");
-		window.core.sendNick("<3");
-		s = 0;
-	
-	}
-
-}, 500);
-
-    
-    
-}, 8000);
 
 
